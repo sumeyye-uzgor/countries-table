da1c6e5f8276eb04258fd76baaaf77d5285cfb57
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-06-14 14:55:16 +0300

    project initialized with a table

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import React,{useEffect, useState } from 'react';
+import axios from "axios"
+import {Table} from "react-bootstrap"
 
-function App() {
+function App ()
+{
+    const [data, setData] = useState([])
+    useEffect( () =>
+    {
+        async function fetchData ()
+        {
+            const {fetchedData} = await axios.get( "https://restcountries.eu/rest/v2/all" )
+            setData(fetchedData)
+        }
+        fetchData()
+    } )
+    console.log(data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="d-flex justify-content-center align-center">
+        <Table striped bordered hover variant="dark" responsive>
+            <thead>
+                <tr>
+                <th>#</th>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Username</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                <td>1</td>
+                <td>Mark</td>
+                <td>Otto</td>
+                <td>@mdo</td>
+                </tr>
+                <tr>
+                <td>2</td>
+                <td>Jacob</td>
+                <td>Thornton</td>
+                <td>@fat</td>
+                </tr>
+                <tr>
+                <td>3</td>
+                <td colSpan="2">Larry the Bird</td>
+                <td>@twitter</td>
+                </tr>
+            </tbody>
+        </Table>
     </div>
   );
 }
